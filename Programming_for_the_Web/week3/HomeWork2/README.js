35774d11ab65f501e692b5c90165e156d58dfcc9
Nikolas Bogucharsky <niktechnopro@gmail.com>
2020-11-09 06:37:21 -0500

    Create README.js

---
+++ Programming_for_the_Web/week3/HomeWork2/README.js
@@ -0,0 +1,11 @@
+## A simple React application to track lists of items (the code is simplified to work with Codio grading system)
+
+### The goals of this assignment
+
+* ### To gain experience understanding and modifying an existing React app
+
+* ### To use the Node framework to create and deploy a React app with multiple components defined in separate files
+
+* ### To gain more experience in implementing callback functions in React components that affect the components’ appearance
+
+* ### To apply what you have learned in the lessons about the relationships between React components and how components interact
